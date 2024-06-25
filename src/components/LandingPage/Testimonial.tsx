5311677decd8f23dff5e24ab9ecc1ced360bb53e
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-06-25 16:51:24 +0530

    WIP working on Testimonials

---
+++ src/components/LandingPage/Testimonial.tsx
@@ -0,0 +1,18 @@
+import "../../styles/LandingPage/Testimonial.css";
+const Testimonial = () => {
+  return (
+    <>
+      <div className="Testimonial_main_Container" data-testid="TestimonialId">
+        <div className="Testimonial_Heading_Div">
+          <h1>What <span>Our Client</span> Say</h1>
+          <h1>about us</h1>
+        </div>
+        <div className="Paragraph_div">
+          <p></p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Testimonial;
--- src/views/LandingPage/LandingPage.tsx
+++ src/views/LandingPage/LandingPage.tsx
@@ -1,12 +1,14 @@
 import AutoSliderimages from "../../components/LandingPage/AutoSliderimages";
 import DeveloperTrial from "../../components/LandingPage/DeveloperTrial";
 import Technologies from "../../components/LandingPage/Technologies";
+import Testimonial from "../../components/LandingPage/Testimonial";
 const LandingPage = () => {
   return (
     <div className="Landing_Page_Main" data-testid="LandingPageID" style={{backgroundColor:"#161E2C"}}>
       <DeveloperTrial />
       <AutoSliderimages />
       <Technologies />
+      <Testimonial/>
     </div>
   );
 };
