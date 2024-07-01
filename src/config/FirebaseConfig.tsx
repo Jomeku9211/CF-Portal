f6d5b582a1af9d09bb16e33c854ac08c3e6915fa
Shivanggour <shivanggour77@gmail.com>
2024-07-01 12:07:36 +0530

    Changes in configuration

--- src/config/FirebaseConfig.tsx
+++ src/config/FirebaseConfig.tsx
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getStorage } from 'firebase/storage';
-import 'firebase/database'; 
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,12 +12,12 @@ const firebaseConfig = {
   projectId: "dodportal",
   storageBucket: "dodportal.appspot.com",
   messagingSenderId: "892588666236",
-  appId: "1:892588666236:web:845b575bd4ddf0f87e08f9"
+  appId: "1:892588666236:web:845b575bd4ddf0f87e08f9",
 };
 
 // Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
 const database = getStorage(app);
-console.log("app setup successfull")
-export {app  , database}
+console.log("app setup successfull");
+export { app, database };
