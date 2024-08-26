a7b875e75a1b0a1c844b0fa4d2d93265cbfbeb0d
yogendrarajput123 <yogendra.coderfarm@gmail.com>
2024-08-26 17:20:56 +0530

    WIP : Red Test Case

--- src/views/LandingPage/LandingPage.tsx
+++ src/views/LandingPage/LandingPage.tsx
@@ -1,13 +1,17 @@
 import AutoSliderimages from "../../components/LandingPage/AutoSliderimages";
 import ContactUs from "../../components/LandingPage/ContactUs";
 import DeveloperTrial from "../../components/LandingPage/DeveloperTrial";
+import FreeTrial from "../../components/LandingPage/FreeTrial";
 // import HiringFlow from "../../components/LandingPage/HiringFlow";
 // import Technologies from "../../components/LandingPage/Technologies";
 import Testimonial from "../../components/LandingPage/Testimonial";
+
+
 const LandingPage = () => {
   return (
     <div className="Landing_Page_Main" data-testid="LandingPageID" style={{backgroundColor: "#FAFAFA"}}>
       <DeveloperTrial />
+      <FreeTrial />
       {/* <Technologies /> */}
       {/* <HiringFlow /> */}
       <Testimonial/>
